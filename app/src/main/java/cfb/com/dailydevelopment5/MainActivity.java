fdf0b479a4784292f86463131efb43095ab3752f
caofengbin <1050430934@qq.com>
2017-07-29 20:33:16 +0800

    init projects

---
+++ app/src/main/java/cfb/com/dailydevelopment5/MainActivity.java
@@ -0,0 +1,78 @@
+package cfb.com.dailydevelopment5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private String[] mainItems;
+    private ListView mMainListView;
+    private ArrayAdapter<String> itemAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainItems = getResources().getStringArray(R.array.main_view_string_array);
+        mMainListView = (ListView) findViewById(R.id.main_activity_listView);
+        itemAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mainItems);
+
+        mMainListView.setAdapter(itemAdapter);
+        mMainListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        switch (position) {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+        }
+    }
+
+    private void startIntent(Class class1) {
+        Intent intent = new Intent(MainActivity.this, class1);
+        startActivity(intent);
+    }
+}
